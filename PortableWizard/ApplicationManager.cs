9fdb62ab82da052c6c79a83bc8a08585298cd493
Gábor Szalóki <gigu92@gmail.com>
2014-10-16 19:17:01 +0200

    wizard + extended wpf toolkit

---
+++ PortableWizard/ApplicationManager.cs
@@ -0,0 +1,16 @@
+﻿using PortableWizard.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortableWizard
+{
+	class ApplicationManager
+	{
+		private string AppsPath;
+		
+
+	}
+}
---
+++ PortableWizard/Model/Application.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortableWizard.Model
+{
+	class Application
+	{
+		public string Name { get; set; }
+		public bool IsDesktopShortcut { get; set; }
+		public bool IsStartMenuShortcut { get; set; }
+		public bool IsPinnedToStart { get; set; }
+		public bool IsPinnedToTaskbar { get; set; }
+		public bool IsStartup { get; set; }
+		public List<string> SupportedFileExtensions { get; set; }
+		public List<string> HandledFileExtensions { get; set; }
+
+		public Application() {	}
+		
+	}
+}
