0aef607513cdced861a7bf9bd12ab25de0e43e5d
Törcsvári Gergő <torcsvari.gergo@gmail.com>
2014-11-12 19:14:54 +0100

    new icons and appini

--- PortableWizard/postbuild.bat
+++ PortableWizard/postbuild.bat
@@ -2,5 +2,8 @@ echo "postbuild-start on %~1"
 
 xcopy "%~2PortableWizard.exe" "%~1"
 xcopy "%~2*" "%~1App\bin" /s /i
+xcopy "%~2\..\..\Media\appinfo.ini" "%~1App\AppInfo"
+copy "%~2\..\..\Media\PortableWizard_icon.ico" "%~1App\AppInfo\appicon.ico"
+copy "%~2\..\..\Media\PortableWizard_logo.png" "%~1App\AppInfo\appicon_32.png"
 
 echo "postbuild-end"
